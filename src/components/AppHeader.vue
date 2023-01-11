d212e59afcc4c9eac0e5e683c74334738c7af69c
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-01-11 16:19:26 +0100

    add title

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,26 @@
 <script>
 export default {
-
+    name: 'AppHeader'
 }
 </script>
 
 <template>
-    <h1 class="text-danger">Header</h1>
+    <header>
+        <div class="row">
+            <div class="col-6 d-flex p-3">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg" alt="logo">
+                <h1 class="fs-4">Yu-Gi-Oh Api</h1>
+            </div>
+        </div>
+    </header>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+header {
+    height: 80px;
 
+    img {
+        width: 60px;
+    }
+}
 </style>
