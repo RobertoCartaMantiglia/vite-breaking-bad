305592d02e71a83f9cea6d1e5f38129668c92d7c
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-01-11 17:39:18 +0100

    create axios call

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,36 @@
 <script>
 import { store } from '../store.js';
+import axios from 'axios';
 
 export default {
     data() {
         return {
             store,
+            cardsList: [],
         }
+    },
+    methods: {
+        getCard() {
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0 ', {
+                params: {
+                    ID: 12345
+                }
+            })
+                .then((response) => {
+                    console.log(response.data.data);
+                    this.cardsList = response.data.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .finally(function () {
+                    // always executed
+                });
+        }
+    },
+
+    created() {
+        this.getCard()
     },
 }
 </script>
