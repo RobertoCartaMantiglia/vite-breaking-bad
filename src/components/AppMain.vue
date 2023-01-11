9c7f3a470b60fc6e259e1b9a24183dd1fbacfc47
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-01-11 17:41:40 +0100

    add number of cards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,7 +11,7 @@ export default {
     },
     methods: {
         getCard() {
-            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0 ', {
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=15&offset=0 ', {
                 params: {
                     ID: 12345
                 }
