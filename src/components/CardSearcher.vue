ebdbfc6fc080010d518874b566b8b85c40549554
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-01-12 17:29:24 +0100

    change the names in option select

--- src/components/CardSearcher.vue
+++ src/components/CardSearcher.vue
@@ -16,9 +16,13 @@ export default {
         <label for="" class="form-label">archetypes</label>
         <select class="form-select" @change="$emit('changeArchetype')" v-model="store.serchArchetype" name="" id="">
             <option selected>Select one</option>
-            <option value="">New Delhi</option>
-            <option value="">Istanbul</option>
-            <option value="">Jakarta</option>
+            <option value="Alien">Alien</option>
+            <option value="Laval">Laval</option>
+            <option value="Inzektor">Inzektor</option>
+            <option value="Umi">Umi</option>
+            <option value="Gusto">Gusto</option>
+
+
         </select>
     </div>
 </template>
